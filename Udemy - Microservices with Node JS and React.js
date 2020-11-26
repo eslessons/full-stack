01dd626e2a9c2a492a6f58258c7c491074ce3ac8
ES Lessons <eslessons@tutanota.com>
2020-11-26 11:41:12 +0300

    first commit

---
+++ Udemy - Microservices with Node JS and React.js
@@ -0,0 +1,44 @@
+/* Udemy - Microservices with Node JS and React
+
+01 Fundamental Ideas Around Microservices
+Start
+Finish
+
+02 A Mini-Microservices App
+03 Running Services with Docker
+04 Orchestrating Collections of Services with Kubernetes
+05 Architecture of Multi-Service Apps
+06 Leveraging a Cloud Environment for Development
+07 Response Normalization Strategies
+08 Database Management and Modeling
+09 Authentication Strategies and Options
+10 Testing Isolated Microservices
+11 Integrating a Server-Side-Rendered React App
+12 Code Sharing and Reuse Between Services
+13 Create-Read-Update-Destroy Server Setup
+14 NATS Streaming Server - An Event Bus Implementation
+15 Connecting to NATS in a Node JS World
+16 Managing a NATS Client
+17 Cross-Service Data Replication In Action
+18 Understanding Event Flow
+19 Listening for Events and Handling Concurrency Issues
+20 Worker Services
+21 Handling Payments
+22 Back to the Client
+23 CI-CD
+24 [Appendix A] - Basics of Docker
+25 [Appendix B] - Basics of Typescript
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
